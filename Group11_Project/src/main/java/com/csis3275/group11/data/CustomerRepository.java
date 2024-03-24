379d590b43dcc7aeacf93086e56d98fee59a596b
ldt <148156504+mwei2@users.noreply.github.com>
2024-03-24 15:16:32 -0700

    Delete Group11_Project directory
    wrong folder uploaded. Now one coming...

--- Group11_Project/src/main/java/com/csis3275/group11/data/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.csis3275.group11.data;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.csis3275.group11.model.Customer;
-
-public interface CustomerRepository extends CrudRepository<Customer, Long>{
-
-}
