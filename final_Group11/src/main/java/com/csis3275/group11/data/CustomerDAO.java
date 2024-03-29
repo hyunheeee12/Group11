0264f63c03ed92c8da346e2b0fc382ea8ffadc36
hyunheeee12 <kimh134@student.douglascollege.ca>
2024-03-28 21:37:12 -0700

    this is the final version of software

---
+++ final_Group11/src/main/java/com/csis3275/group11/data/CustomerDAO.java
@@ -0,0 +1,34 @@
+package com.csis3275.group11.data;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.csis3275.group11.model.Customer;
+
+@Service
+public class CustomerDAO 
+{
+	@Autowired
+	private CustomerRepository customerRepository;
+	
+	public Customer createCustomer(Customer newCustomer)
+	{
+		return customerRepository.save(newCustomer);
+	}
+	
+	public List<Customer> listAllCustomers()
+	{
+		return (List<Customer>)customerRepository.findAll();
+	}
+	
+	public Customer findByEmail(String email) {
+	    return customerRepository.findByEmail(email);
+	}
+	
+	public Customer findByLicence(String licence)
+	{
+		return customerRepository.findByLicence(licence);
+	}
+}
